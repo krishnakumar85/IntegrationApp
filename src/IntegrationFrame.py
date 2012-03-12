ad566162d2f8767b2502c1ca96bca2b3cead72b3
Krishna <krishnakumar85+github@gmail.com>
2012-03-13 01:05:37 +0530

    Added toolbar and statusbar (non-functional) FIXME: Statusbar

--- src/IntegrationFrame.py
+++ src/IntegrationFrame.py
@@ -1,49 +1,80 @@
 import wx
 import Process
+import images
+
+ID_PLAY = wx.NewId()
+ID_STOP = wx.NewId()
 
 
 class IntegrationFrame(wx.Frame):
-    def __init__(self, parent, fid, title, steps):
+    def __init__(self, parent, fid, title, sequencer):
+        self.sequencer = sequencer
+
         wx.Frame.__init__(self, parent, fid, title=title, size=wx.DefaultSize)
         icon = wx.Icon("..\img\icon.ico", wx.BITMAP_TYPE_ICO)
         self.SetIcon(icon)
-        
-        panel = wx.Panel(self, size=(1000, 1000))
-        
+
+        panel = wx.Panel(self, -1)
+
+        self.BuildToolBar()
+        self.statusbar = self.CreateStatusBar()
+
         self.Bind(wx.EVT_CLOSE, self.onCloseWindow)
-        panel.Bind(Process.ProcessEvent.EVT_ASK_ON_FAILURE, self.AskOnFailure)
         panel.Bind(Process.ProcessEvent.EVT_STEP_SUCCESS, self.StepSuccess)
-        
+        self.Bind(Process.ProcessEvent.EVT_STATUS_UPDATE, self.OnStatusUpdate)
+
         sizer = wx.BoxSizer(wx.VERTICAL)
+        self.statusbar.SetStatusText("Ready.")
         # Addition of process buttons
         self.processButtons = []
-        for this_step in steps:
+        for this_step in sequencer.steps:
             print this_step.stepname
             thisbtn = wx.Button(panel, label=this_step.stepname,
                                 size=(100, 30))
             this_step.widget = thisbtn
             this_step.panel = panel
+            this_step.frame = self
             # Call process specific steps
-            self.Bind(wx.EVT_BUTTON, this_step.EntryPoint, thisbtn)
+            self.Bind(wx.EVT_BUTTON, this_step.EntryPointWrapper, thisbtn)
             sizer.Add(thisbtn)
-            
+
             # See if the objectives of the step are met. It met, show in green.
             if this_step.PostCondition() == 0:
-                thisbtn.SetLabel(thisbtn.GetLabel()+" [Done]")
+                thisbtn.SetLabel(thisbtn.GetLabel() + " [Done]")
                 thisbtn.SetBackgroundColour('yellow')
 
         self.SetSizerAndFit(sizer)
 
-    def AskOnFailure(self, event):
-        dlg = wx.MessageDialog(None, event.msg, "Failed", wx.YES_NO |
+    def BuildToolBar(self):
+        toolbar = self.CreateToolBar()
+        toolbar.AddSimpleTool(ID_PLAY, images.GetBitmap_Play_png(), "Start",
+                              "Start with integration")
+        #toolbar.AddTool(ID_PLAY, images.GetBitmap_Play_png(), isToggle=True)
+        wx.EVT_TOOL(self, ID_PLAY, self.OnPlay)
+        toolbar.Realize()
+
+    def OnPlay(self, event):
+        print "OnPlay Play Clicked"
+        self.sequencer.start()
+        self.statusbar.SetStatusText("Completed.")
+
+    def OnStatusUpdate(self, event):
+        print "OnStatusUpdate", event.status_msg
+        self.statusbar.SetStatusText(event.status_msg)
+        self.statusbar.Refresh()
+        self.statusbar.Update()
+
+    @staticmethod
+    def AskOnFailure(msg):
+        print "AskOnFailure", msg
+        dlg = wx.MessageDialog(None, msg, "Failed", wx.YES_NO |
                                wx.ICON_QUESTION)
         retcode = dlg.ShowModal()
-        print "INSIDE Ask Dialog"
-        if retcode == wx.ID_YES:
-            wx.CallAfter(event.reply, True)
-        else:
-            wx.CallAfter(event.reply, False)
         dlg.Destroy()
+        if retcode == wx.ID_YES:
+            return True
+        else:
+            return False
 
     def StepSuccess(self, event):
         event.widget.SetBackgroundColour(event.colour)
